3c26a45e918d947aa87809aa0deea314343b08d9
= <=>
2022-04-02 05:19:33 +0700

    3

--- nsh_site/settings/prod.py
+++ nsh_site/settings/prod.py
@@ -1,4 +1,5 @@
 from distutils.command.config import config
+from time import asctime
 
 from nsh_site.nsh_site.settings.dev import ALLOWED_HOSTS, DEBUG
 from .base import *
@@ -12,6 +13,35 @@ DEBUG = False
 ALLOWED_HOSTS = [
     'nshoa99-blog.herokuapp.com',
 ]
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'MYAPP': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
 
 django_on_heroku.settings(locals, staticfiles=False)
 del DATABASES['default']['OPTIONS']['sslmode']
