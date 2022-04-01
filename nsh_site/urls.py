fbee67d9c88afa8022d7e6485500f36b71ba224f
= <=>
2022-04-02 05:29:39 +0700

    change requirements

--- nsh_site/urls.py
+++ nsh_site/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')) #http://localhost:8000/
-    # path('blog/', include('blog.urls')) #http://localhost:8000/blog/
+    path('', include('blog.urls')) 
+    # path('blog/', include('blog.urls')) 
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
